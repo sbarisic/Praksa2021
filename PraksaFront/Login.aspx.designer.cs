805f2ec72143d7abcd5d43dc83a0b84d62e3e596
filip.krmpotic <filip.krmpotic@serengetitech.com>
2021-06-17 08:46:59 +0200

    ....

--- PraksaFront/Login.aspx.designer.cs
+++ PraksaFront/Login.aspx.designer.cs
@@ -76,5 +76,14 @@ namespace PraksaFront
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCancel;
+
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
     }
 }
