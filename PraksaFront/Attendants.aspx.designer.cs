48e1f1033b442db7ee6e188359d8f77766ef5997
Romano Marinic <romano.marinic@serengetitech.com>
2021-06-23 14:48:07 +0200

    Updated Attendants

--- PraksaFront/Attendants.aspx.designer.cs
+++ PraksaFront/Attendants.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace PraksaFront
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater attendanceRepeater;
+
+        /// <summary>
+        /// submitButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button submitButton;
     }
 }
