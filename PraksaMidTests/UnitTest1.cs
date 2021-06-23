fbf868746ebcf72da258d851f73582f4e2be58c1
Filip Krmpotić <fkrmpotic@vub.hr>
2021-06-23 21:00:53 +0200

    added login tests

--- PraksaMidTests/UnitTest1.cs
+++ PraksaMidTests/UnitTest1.cs
@@ -2,14 +2,15 @@
 using System;
 using PraksaMid;
 using PraksaMid.Model;
-
+using System.Configuration;
+using PraksaFront;
 
 namespace PraksaMidTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
-        
+        String connectionString = "Data Source=167.86.127.239;Initial Catalog=Praksa2021;User ID=SerengetiUser;Password=Serengeti12345678910";
 
         [TestInitialize]
         public void Setup()
@@ -35,19 +36,29 @@ namespace PraksaMidTests
         [TestMethod]
         public void TestLoginIsSuccessful()
         {
-
+            var result = Authentication.LogIn(connectionString, "user1@gmail.com", "useruser");
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod]
-        public void TestLoginIsNotSuccessful()
+        public void TestLoginIsNotSuccessfulWrongUser()
         {
+            var result = Authentication.LogIn(connectionString, "wrong_user@gmail.com", "useruser");
+            Assert.AreEqual(0, result);
+        }
 
+        [TestMethod]
+        public void TestLoginIsNotSuccessfulWrongPassword()
+        {
+            var result = Authentication.LogIn(connectionString, "user@gmail.com", "wrong_password");
+            Assert.AreEqual(0, result);
         }
 
         [TestMethod]
         public void TestGetAttendants()
         {
-
+            var result = Attendant.GetAttendants(connectionString, 1);
+            Assert.IsTrue(result.Count > 0);
         }
 
         [TestMethod]
