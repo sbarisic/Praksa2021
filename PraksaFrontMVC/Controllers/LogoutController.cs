34a8431e19464294d77402bd1503237478c6fa14
Romano Marinic <romano.marinic@serengetitech.com>
2021-06-30 09:52:25 +0200

    Added Calendar

--- PraksaFrontMVC/Controllers/LogoutController.cs
+++ PraksaFrontMVC/Controllers/LogoutController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PraksaFrontMVC.Controllers
 {
-    public class Registration : Controller
+    public class LogoutController : Controller
     {
         public IActionResult Index()
         {
