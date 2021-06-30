465ae5170a13071e1e799af45ef60203708782d9
Klara Koščević <klara.koscevic@serengetitech.com>
2021-06-30 09:42:25 +0200

    Get rolenames

---
+++ PraksaFrontMVC/Data/RoleNameData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using PraksaFrontMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraksaFrontMVC.Data
+{
+    public static class RoleNameData
+    {
+        public static Task<List<RoleName>> GetRoleNames()
+        {
+            List<RoleName> roles = new();
+
+            SqlConnection con = ConnectionString.ConStr();
+            con.Open();
+
+            SqlCommand cmd = new("getRoleNames", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr != null)
+            {
+                while (dr.Read())
+                {
+                    RoleName roleName = new()
+                    {
+                        Id = Convert.ToInt32(dr["ID"]),
+                        Name = dr["Naziv uloge"].ToString()
+                    };
+
+                    roles.Add(roleName);
+                }
+            }
+            return Task.FromResult(roles);
+        }
+
+    }
+}
