6852182d355b69f7ebe8a44cace85f29b294af1d
Klara Koščević <klara.koscevic@serengetitech.com>
2021-07-01 11:34:30 +0200

    Added mid par attendant

--- PraksaFrontMVC/Models/Attendant.cs
+++ PraksaFrontMVC/Models/Attendant.cs
@@ -13,6 +13,10 @@ namespace PraksaFrontMVC.Models
         public int IdInteres { get; set; }
         public int IdAttendance { get; set; }
         public string SelectionTime { get; set; }
-
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string Job { get; set; }
+        public string Interes { get; set; }
+        public string Attendance { get; set; }
     }
 }
